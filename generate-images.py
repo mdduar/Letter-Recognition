1adcaa1060f7f1834abdb132e709068e99fae7b0
Michael Duarte <michael.d.duarte@gmail.com>
2023-10-27 21:26:06 -0400

    add font path as an option

--- generate-images.py
+++ generate-images.py
@@ -1,6 +1,7 @@
 import argparse
 from PIL import Image, ImageDraw, ImageFont
 import string
+import os
 
 ## BLUE 55 182 246
 
@@ -10,16 +11,21 @@ import string
 
 ## GREEN 53 212 97
 
+## PURPLE 136 47 246
+
 def main():
   parser = argparse.ArgumentParser(
     description='Generate Images',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-  
   parser.add_argument('--dimensions','-d', type=int, nargs=2,
                         default=[1920, 1080],
                         metavar="N N",
                         help="Dimensions for the images")
+  
+  parser.add_argument('--font-path','-fp', type=str,
+                        default=None,
+                        help="Path to font")
   
   parser.add_argument('--bgcolor','-bg', type=int, nargs=3,
                         default=[255, 255, 255],
@@ -56,20 +62,33 @@ def main():
     characters.extend([str(x) for x in range(10)])
 
   for x in characters:
-    generate_image(x, args.dimensions, f'output/{x}.png', args.font_size, tuple(args.bgcolor), tuple(args.fgcolor))
+    generate_image(x, args.dimensions, f'output/{x}.png', args.font_path, args.font_size, tuple(args.bgcolor), tuple(args.fgcolor))
 
+def get_font_path(path):
+  if path:
+    return path
+  
+  fonts = [f for f in os.listdir('fonts/') if os.path.isfile(os.path.join('fonts',f)) and f != '.gitkeep']
+  if len(fonts):
+    return os.path.join('fonts',fonts[0])
+  
+  return None
 
-def generate_image(character, dimensions, file_name, font_size, bgcolor, fgcolor):
+def generate_image(character, dimensions, file_name, font_path, font_size, bgcolor, fgcolor):
   image = Image.new('RGB', dimensions, bgcolor)
 
-  font = ImageFont.truetype("/home/mdduar/.local/share/fonts/Monotype/OpenType/Sassoon Infant Std/Sassoon_Infant_Std_Bold.otf", font_size)
+  font_path = get_font_path(font_path)
+  if font_path == None:
+    font = ImageFont.load_default(font_size)
+
+  else:
+    font = ImageFont.truetype(font_path, font_size)
 
   draw = ImageDraw.Draw(image)
 
   text_dimensions = draw.textbbox((0,0), character, font, anchor="lt")
 
   coord = tuple((dimensions[x] - text_dimensions[x+2]) // 2 for x in range(2))
-  print(coord)
 
   draw.text(coord, character, fill=fgcolor, font=font, anchor="lt")
 
